a062837610f4343f52f81e6b7b062916c5b4b63c
Hassan Rahman <hr-awesome@hotmail.com>
2024-02-20 17:46:57 -0500

    Update of forecast titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { ForecastWindowComponent } from './forecast-window/forecast-window.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ForecastWindowComponent],
+  imports: [CommonModule, RouterOutlet, ForecastWindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
